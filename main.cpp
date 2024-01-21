ed6db90a11e760fbe32029f3e70a971f1d0635e1
4Dimensional <68523111+4Dimensional@users.noreply.github.com>
2024-01-21 18:46:57 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include "sg3d.h"
 
-Graphics g(800, 600, "SG3D - Playground");
-sg3d g3d;
+Graphics g(800, 600, "SG3D - Playground"); // Create a new SG graphic
+sg3d g3d; // Create a new SG3D graphic
 
-sg3d::cam camera = {
-    0.0f,
-    0.0f,
-    0.0f,
-    0.0f,
-    -3.0f,
-    0.0f,
-    0.0f,
-    0.0f,
-    0.0f,
-    1
+sg3d::cam camera = { // Create camera
+    0.0f, // rotation y
+    0.0f, // rotation x
+    0.0f, // rotation z
+    0.0f, // z position
+    -3.0f, // y position
+    0.0f, // x position
+    0.0f, // COY
+    0.0f, // COX
+    0.0f, // COZ
+    1 // Clipping plane
 };
 
+// Some variables for the example
 float lookspeed = 0.01;
 float movespeed = 0.05;
 bool lookleft = false;
@@ -25,26 +26,31 @@ bool walkbackward = false;
 bool strafeleft = false;
 bool straferight = false;
 
+// Create a position to summon the pillar and cube model
 sg3d::tri pillar1 = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, g._RGB(0, 0, 0)};
 sg3d::tri cube1 = {{2, 5, 0}, {2, 5, 0}, {2, 5, 0}, {0, 0, 0}, {0, 0, 0}, g._RGB(0, 0, 0)};
 
+// Import the pillar and cube model
 std::vector<sg3d::tri> pillar_object1 = g3d.load_object("src/models/complex/pillar.sg3d", pillar1, g);
 std::vector<sg3d::tri> cube_object1 = g3d.load_object("src/models/primitive/cube.sg3d", cube1, g);
 
 void Render()
 {
-    g3d.setCurrentCamera(camera);
+    // SG 2d render function
+    
+    g3d.setCurrentCamera(camera); // Set the camera
 
-    std::vector<sg3d::obj> world = {
+    std::vector<sg3d::obj> world = { // Create the world
         g3d.pack_tris( pillar_object1 ),
         g3d.pack_tris( cube_object1 )
     };
 
-    g3d.drawWorld(world, g);
+    g3d.drawWorld(world, g); // Draw the world
 
-    g._DrawLine(400, 290, 400, 310, g._RGB(255, 0, 0));
+    g._DrawLine(400, 290, 400, 310, g._RGB(255, 0, 0)); // Some SG 2d lines drawn
     g._DrawLine(390, 300, 410, 300, g._RGB(255, 0, 0));
 
+    // Simple camera controls
     if (lookleft)
         camera.roy -= lookspeed;
     if (lookright)
@@ -73,6 +79,8 @@ void Render()
 
 void Keyboard(KeySym ks)
 {
+    // SG 2d keyboard inputs
+    
     switch (ks)
     {
         case XK_Right:
@@ -99,8 +107,11 @@ void Keyboard(KeySym ks)
 
 int main(int argc, char** argv)
 {
+    // Birth
     g._InitWindow();
+    // Life
     g._WindowLoop(Render, Keyboard);
+    // Death
     g._CloseWindow();
     return 0;
 }
